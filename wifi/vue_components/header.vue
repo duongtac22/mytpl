126a281f96e12f591a49c39ee764943f4ed8f1d3
duongta <duongtac22@gmail.com>
2020-12-04 17:39:54 +0700

    asdlaksdjasd

---
+++ wifi/vue_components/header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header id="header">
+        <div class="admin-container align-items-center container-fluid d-flex">
+            <div class="d-flex justify-content-between main-content w-100">
+                <a href="/" class="logo-wifi">
+                    <img src="../../images/logo-bizfly-wifi.png" alt="">
+                </a>
+
+                <div class="header-top-right float-right d-flex align-items-center">
+                    <div class="dropdown dropdown mr-3">
+                        <a href="/" class="notification" >
+                            <img src="../../images/bizfly-ic-bell.png" alt="" class="noti-ic" />
+                        </a>
+                    </div>
+                    <div class="dropdown info d-flex align-items-center">
+                        <a class="" href="#" >
+                            <img src="../../images/bizfly-login-ava.png" alt="" class="avatar" />
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
