1e46e450049aef18d2d9e4263a8b73f36e3fa0a1
duongta <duongtac22@gmail.com>
2021-01-13 17:58:45 +0700

    asdasd

--- pokemon-vue/src/App.vue
+++ pokemon-vue/src/App.vue
@@ -1,32 +1,108 @@
 <template>
  <div class="p-14 ">
-    <router-link class="flex justify-center text-4xl text-yellow-700" to="/"
+    <!-- <router-link class="flex justify-center text-4xl text-yellow-700" to="/"
       >Pokemon Picker</router-link
     >
-    <router-view />
+    <router-view /> -->
+    <div id="app">
+       <h1>Pokemon's</h1>
+        <Home></Home>
+    </div>
   </div>
   
 </template>
-
+<script>
+import Home from './views/Home.vue';
+export default {
+  name : 'App',
+  components: {
+    Home
+  }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css?family=Acme');
+
+
+
+
+body {
+  background: #00303f;
+  line-height: 1.5;
+  font-family: 'Acme', arial;
+}
+h1 { color: #efefef; text-align: center;}
+.cart-wrapper {
   text-align: center;
-  color: #2c3e50;
+  margin: 20px 0;
+  padding: 14px;
+  max-width: 1100px;
+  margin: auto;
 }
 
-#nav {
-  padding: 30px;
+.cart-wrapper > h1 {
+  text-align: center;
+  color: #ffffff;
+  margin-bottom: 20px;
+  font-size: 20px;
+  text-transform: uppercase;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.cart-wrapper  {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 30px;
+  justify-content: center;
+  align-items: center;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.Card {
+  width: 120px auto;
+  background: #5bb0ca;
+  color: #e4c439;
+  padding: 1rem;
+  border-radius: 10px;
+  border-top: 0.5px solid #e4c439;
+  border-bottom: 0.5px solid #e4c439;
+  text-align: center;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  transition: 0.2s ease-in-out all;
 }
+
+.Card:hover {
+  transform: scale(1.05);
+}
+
+.Card--id {
+  background: #e4c439;
+  width: 3rem;
+  color: #fff;
+  padding: 0.1rem;
+  font-weight: 700;
+  position: absolute;
+  border-radius: 0 0 10px 0;
+  top: 0;
+  left: 0;
+}
+.Card--name {
+  text-transform: capitalize;
+  color: rgb(255, 255, 255);
+  font-size: 2rem;
+  font-weight: 700;
+}
+
+.Card--image {
+  width: 150px;
+  display: block;
+  margin: auto;
+}
+
+.Card--details {
+  font-size: 1.3rem;
+  color: #e4c439;
+}
+
 </style>
--- pokemon-vue/src/views/Home.vue
+++ pokemon-vue/src/views/Home.vue
@@ -1,34 +1,109 @@
 <template>
-  <div class="mt-10 p-4 flex flex-wrap flex-col">
+  <div class="cart-wrapper">
     <div
-      class="ml-4 text-2xl text-blue-400 underline"
-      v-for="(pokemon, idx) in pokemons"
-      :key="idx"
+      class="Card"
+      v-for="(pokemon, idx) in responseName"
+      :key="{ idx }"
+      @click="openDetail"
     >
-        
-        <router-link :to="`/pokemon/${idx + 1}`">
-        {{ pokemon.name }}
-        </router-link>
-      
+      <span class="Card--id">#{{ pokemon.id }}</span>
+      <img
+        class="Card--image"
+        :src="pokemon.sprites.front_default"
+        :alt="pokemon.name"
+      />
+      <h1 className="Card--name">{{ pokemon.name }}</h1>
+      <span className="Card--details">
+        {{ pokemon.types.map((poke) => poke.type.name).join(", ") }}
+      </span>
     </div>
+
+    <!-- <input type="hidden" id="checkbox" v-model="nextUrl" > -->
+    <button @click="newUrl = nextUrl">Load more Pokemon</button>
   </div>
 </template>
 
 <script>
-import { reactive , toRefs} from "vue";
+import { reactive, toRefs, onMounted, watch } from "vue";
 export default {
-  name: 'Home',
+  name: "Home",
   setup() {
-     const pokemonList = reactive({
-      pokemons: []
+    //  const pokemonList = reactive({
+    //   pokemons: []
+    // });
+    //ở trong vue3 có hàm onMounted
+    // đây là đống a muốn fetch khi vào trang đúng k ?   đúng . nhưng lấy xong link này nó phải lấy cái name để lấy thêm thông tin
+    // oke bh e code kiểu cũ cho a xem
+    // sau đó e sẽ hướng dẫn a code theo async await
+    // a đọc thêm nhé
+
+    // fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=50")
+    //   .then((res) => {
+    //     // ví dụ ở đâu res.name = name
+    //     const name = res.name
+    //     //theo kiểu cũ là
+    //     fetch(`url?name=${name}`).then(result => {
+    //       console.log(result)
+    //     })
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     pokemonList.pokemons = data.results;
+    // });
+    //commnet dong tren lai a
+    // nhưng bỏ hết đi bh làm kiểu mới
+    const state = reactive({
+      responseName: [],
+      nextUrl: null,
+      newUrl: null,
+      pokemonApi: "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10",
+      showDetail: false,
     });
-    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=50")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        pokemonList.pokemons = data.results;
+    async function getPokemonApi(newUrl, state) {
+      let apiUrl = "";
+      if (newUrl === "") {
+        apiUrl = state.pokemonApi;
+      } else {
+        apiUrl = newUrl;
+      }
+      await fetch(apiUrl)
+        .then((response) => response.json())
+        .then(async (res) => {
+          let pokemonData = res.results;
+          state.nextUrl = res.next;
+          for (let i = 0; i < pokemonData.length; i++) {
+            await getpokemon(pokemonData[i].name, state.responseName);
+          }
+        });
+    }
+    async function getpokemon(pokemonName, data) {
+      await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then((response) => response.json())
+        .then((result) => {
+          // console.log(result);
+          data.push(result);
+        });
+    }
+
+    function openDetail() {
+      state.showDetail = true;
+    }
+
+    function closeDetail() {
+      state.showDetail = false;
+    }
+
+    watch(
+      () => state.newUrl,
+      async () => {
+        await getPokemonApi(state.newUrl, state);
+      }
+    );
+    onMounted(async () => {
+      await getPokemonApi(state.pokemonApi, state);
     });
-    return { ...toRefs(pokemonList) };
-  }
-}
+
+    return { ...toRefs(state), closeDetail, openDetail };
+  },
+};
 </script>
